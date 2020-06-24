a64c06b05d16a4827af81bdaf029fc046f156d0c
Patrik Souza <32416501+Patrikol4@users.noreply.github.com>
2020-06-23 21:07:53 -0300

    Tipos de variáveis básicas do typescript

---
+++ exemplo-2.ts
@@ -0,0 +1,10 @@
+/** Tipos de variáveis no typescript */
+
+var num : number = 10;
+var bool : boolean = true;
+var str : string = 'Qualquer coisa ai';
+
+/** Imprimindo uma mensagem básica no console*/
+var str2 : string = 'Imagine uma mensagem top aqui para ';
+var str3 : string = `${str2} voce!`;
+console.log(str3);
